8b24fd7dc650910c6ac7d84c455cb643392837fb
lucasogil <lucasogil@gmail.com>
2019-08-27 17:34:48 -0300

    Initial Commit

---
+++ src/main/java/br/com/fatec/lever/LeverApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.lever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeverApplication.class, args);
+	}
+
+}
